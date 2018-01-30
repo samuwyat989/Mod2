985039ed787f97dd53096d57b08b44d529228b67
samuwyat989 <samuwyat989@ed.amdsb.ca>
2018-01-29 22:40:23 -0500

    Add project files.

---
+++ Mod2/DrawScreen.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Mod2
+{
+    partial class DrawScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.randCheckBox = new System.Windows.Forms.CheckBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.randColourButton = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.customButton = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.redButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.animationLabel = new System.Windows.Forms.Label();
+            this.animationRange = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.multiplyer = new System.Windows.Forms.NumericUpDown();
+            this.numberOfPoints = new System.Windows.Forms.NumericUpDown();
+            this.customColDialog = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.animationRange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.multiplyer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPoints)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // randCheckBox
+            // 
+            this.randCheckBox.AutoSize = true;
+            this.randCheckBox.Location = new System.Drawing.Point(435, 467);
+            this.randCheckBox.Name = "randCheckBox";
+            this.randCheckBox.Size = new System.Drawing.Size(191, 17);
+            this.randCheckBox.TabIndex = 55;
+            this.randCheckBox.Text = "Randomize colour during animation";
+            this.randCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(658, 181);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(55, 13);
+            this.label12.TabIndex = 54;
+            this.label12.Text = "RANDOM";
+            // 
+            // randColourButton
+            // 
+            this.randColourButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.randColourButton.Location = new System.Drawing.Point(633, 178);
+            this.randColourButton.Name = "randColourButton";
+            this.randColourButton.Size = new System.Drawing.Size(19, 19);
+            this.randColourButton.TabIndex = 53;
+            this.randColourButton.UseVisualStyleBackColor = false;
+            this.randColourButton.Click += new System.EventHandler(this.randColourButton_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(658, 156);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 13);
+            this.label11.TabIndex = 52;
+            this.label11.Text = "CUSTOM";
+            // 
+            // customButton
+            // 
+            this.customButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.customButton.Location = new System.Drawing.Point(633, 153);
+            this.customButton.Name = "customButton";
+            this.customButton.Size = new System.Drawing.Size(19, 19);
+            this.customButton.TabIndex = 51;
+            this.customButton.UseVisualStyleBackColor = false;
+            this.customButton.Click += new System.EventHandler(this.customButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(658, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 48;
+            this.label9.Text = "GREEN";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(658, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "BLUE";
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Location = new System.Drawing.Point(658, 78);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(30, 13);
+            this.redLabel.TabIndex = 44;
+            this.redLabel.Text = "RED";
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.greenButton.Location = new System.Drawing.Point(633, 128);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(19, 19);
+            this.greenButton.TabIndex = 41;
+            this.greenButton.UseVisualStyleBackColor = false;
+            this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.blueButton.Location = new System.Drawing.Point(633, 103);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(19, 19);
+            this.blueButton.TabIndex = 40;
+            this.blueButton.UseVisualStyleBackColor = false;
+            this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.redButton.Location = new System.Drawing.Point(633, 78);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(19, 19);
+            this.redButton.TabIndex = 37;
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(629, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Choose Colour";
+            // 
+            // animationLabel
+            // 
+            this.animationLabel.AutoSize = true;
+            this.animationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.animationLabel.Location = new System.Drawing.Point(15, 488);
+            this.animationLabel.Name = "animationLabel";
+            this.animationLabel.Size = new System.Drawing.Size(414, 20);
+            this.animationLabel.TabIndex = 35;
+            this.animationLabel.Text = "Animates from \'Number of Points\' to this number of points";
+            // 
+            // animationRange
+            // 
+            this.animationRange.Location = new System.Drawing.Point(19, 466);
+            this.animationRange.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.animationRange.Minimum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.animationRange.Name = "animationRange";
+            this.animationRange.Size = new System.Drawing.Size(410, 20);
+            this.animationRange.TabIndex = 34;
+            this.animationRange.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.animationRange.KeyDown += new System.Windows.Forms.KeyEventHandler(this.animationRange_KeyDown);
+            this.animationRange.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.animationRange_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(284, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 20);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Press enter when in one of the boxes to redraw";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Multiplier";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Number of Points";
+            // 
+            // multiplyer
+            // 
+            this.multiplyer.Location = new System.Drawing.Point(145, 15);
+            this.multiplyer.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.multiplyer.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.multiplyer.Name = "multiplyer";
+            this.multiplyer.Size = new System.Drawing.Size(120, 20);
+            this.multiplyer.TabIndex = 30;
+            this.multiplyer.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.multiplyer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.multiplyer_KeyDown);
+            this.multiplyer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.multiplyer_KeyPress);
+            // 
+            // numberOfPoints
+            // 
+            this.numberOfPoints.Location = new System.Drawing.Point(19, 15);
+            this.numberOfPoints.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numberOfPoints.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numberOfPoints.Name = "numberOfPoints";
+            this.numberOfPoints.Size = new System.Drawing.Size(120, 20);
+            this.numberOfPoints.TabIndex = 29;
+            this.numberOfPoints.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numberOfPoints.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numberOfPoints_KeyDown);
+            this.numberOfPoints.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numberOfPoints_KeyPress);
+            // 
+            // DrawScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.randCheckBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.randColourButton);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.customButton);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.greenButton);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.animationLabel);
+            this.Controls.Add(this.animationRange);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.multiplyer);
+            this.Controls.Add(this.numberOfPoints);
+            this.Name = "DrawScreen";
+            this.Size = new System.Drawing.Size(770, 527);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.animationRange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.multiplyer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPoints)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox randCheckBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button randColourButton;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button customButton;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label animationLabel;
+        private System.Windows.Forms.NumericUpDown animationRange;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown multiplyer;
+        private System.Windows.Forms.NumericUpDown numberOfPoints;
+        private System.Windows.Forms.ColorDialog customColDialog;
+    }
+}
---
+++ Mod2/DrawScreen.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mod2
+{
+    public partial class DrawScreen : UserControl
+    {
+        public DrawScreen()
+        {
+            InitializeComponent();
+        }
+
+        int pointsNum, multiply, radius = 200, p2, anRnge, R, G, B;
+        double cycle;
+        Random rand = new Random();
+        Point origin = new Point(300, 250);
+        Point[] drawPoints;
+        Graphics g, g2;
+        Bitmap bm;
+        Color drawColour;
+        List<Button> colourButtons;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            bm = new Bitmap(this.Width, this.Height);
+            g2 = Graphics.FromImage(bm);
+            drawColour = Color.Red;
+            colourButtons = new List<Button>(new Button[] { redButton, greenButton, blueButton, customButton, randColourButton });
+        }
+
+        private void numberOfPoints_KeyPress(object sender, KeyPressEventArgs e)
+        {         
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) //only allows numbers to be typed
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void multiplyer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void animationRange_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void numberOfPoints_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                pointsNum = Convert.ToInt32(numberOfPoints.Value);
+                multiply = Convert.ToInt32(multiplyer.Value);
+                FindPoints();
+                DrawMod();
+            }
+        }
+
+        private void multiplyer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                pointsNum = Convert.ToInt32(numberOfPoints.Value);
+                multiply = Convert.ToInt32(multiplyer.Value);
+                FindPoints();
+                DrawMod();
+            }
+        }
+
+        private void animationRange_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                pointsNum = Convert.ToInt32(numberOfPoints.Value);
+                multiply = Convert.ToInt32(multiplyer.Value);
+                anRnge = Convert.ToInt32(animationRange.Value) - pointsNum;
+                for (int i = 0; i <= anRnge; i++)
+                {
+                    FindPoints();
+                    DrawMod();
+                    pointsNum++;
+                }
+                randCheckBox.Checked = false;
+            }
+        }
+
+        public void FindPoints()
+        {
+            drawPoints = new Point[pointsNum];
+
+            for (int i = 0; i < pointsNum; i++)//Works in radians 
+            {
+                drawPoints[i] = new Point(Convert.ToInt32(origin.X + (Math.Cos((2 * Math.PI / pointsNum) * i)) * radius),
+                     Convert.ToInt32(origin.Y - (Math.Sin((2 * Math.PI / pointsNum) * i)) * radius));
+            }
+        }
+
+        public void DrawMod()
+        {
+            Pen drawPen = new Pen(drawColour);
+            Pen randPen;
+            g2.Clear(Color.WhiteSmoke);
+
+            for (int i = 0; i < pointsNum; i++)
+            {
+                g2.DrawRectangle(drawPen, drawPoints[i].X, drawPoints[i].Y, 1, 1);
+            }
+
+            for (int i = 0; i < pointsNum; i++)
+            {
+                p2 = i * multiply;
+                cycle = p2 / pointsNum;
+                if (p2 > pointsNum - 1)
+                {
+                    p2 = p2 - Convert.ToInt32(Math.Floor(cycle) * pointsNum);
+                }
+                if (randCheckBox.Checked == true)
+                {
+                    RandomizeColour();
+                    randPen = new Pen(Color.FromArgb(R, G, B));
+                    g2.DrawLine(randPen, drawPoints[i].X, drawPoints[i].Y, drawPoints[p2].X, drawPoints[p2].Y);
+                }
+                else { g2.DrawLine(drawPen, drawPoints[i].X, drawPoints[i].Y, drawPoints[p2].X, drawPoints[p2].Y); }
+            }
+            g.DrawImage(bm, 0, 0);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g.DrawImage(bm, 0, 0);
+        }
+
+        #region Colour Buttons
+        private void redButton_Click(object sender, EventArgs e)
+        {
+            drawColour = Color.Red;
+            redButton.BackColor = drawColour;
+            FindPoints();
+            DrawMod();
+            for (int i = 0; i < colourButtons.Count(); i++)
+            {
+                if (i != colourButtons.IndexOf(redButton)) { colourButtons[i].BackColor = Color.Gainsboro; }
+            }
+        }
+        private void greenButton_Click(object sender, EventArgs e)
+        {
+            drawColour = Color.Green;
+            greenButton.BackColor = drawColour;
+            FindPoints();
+            DrawMod();
+            for (int i = 0; i < colourButtons.Count(); i++)
+            {
+                if (i != colourButtons.IndexOf(greenButton)) { colourButtons[i].BackColor = Color.Gainsboro; }
+            }
+        }
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+            drawColour = Color.Blue;
+            blueButton.BackColor = drawColour;
+            FindPoints();
+            DrawMod();
+            for (int i = 0; i < colourButtons.Count(); i++)
+            {
+                if (i != colourButtons.IndexOf(blueButton)) { colourButtons[i].BackColor = Color.Gainsboro; }
+            }
+        }
+        private void customButton_Click(object sender, EventArgs e)
+        {
+            customColDialog.ShowDialog();
+            drawColour = customColDialog.Color;
+            customButton.BackColor = drawColour;
+            FindPoints();
+            DrawMod();
+            for (int i = 0; i < colourButtons.Count(); i++)
+            {
+                if (i != colourButtons.IndexOf(customButton)) { colourButtons[i].BackColor = Color.Gainsboro; }
+            }
+        }
+        private void randColourButton_Click(object sender, EventArgs e)
+        {
+            RandomizeColour();
+            randColourButton.BackColor = drawColour;
+            FindPoints();
+            DrawMod();
+            for (int i = 0; i < colourButtons.Count(); i++)
+            {
+                if (i != colourButtons.IndexOf(randColourButton)) { colourButtons[i].BackColor = Color.Gainsboro; }
+            }
+        }
+        public void RandomizeColour()
+        {
+            R = rand.Next(1, 256);
+            G = rand.Next(1, 256);
+            B = rand.Next(1, 256);
+            drawColour = Color.FromArgb(R, G, B);
+        }
+        #endregion
+    }
+}
